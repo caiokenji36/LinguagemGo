989a093313c507df527b8f7e425ce784aecf0de1
Caio Kenji <caio.kenji36@gmail.com>
2020-03-28 11:10:27 -0300

    Att

--- hello.go
+++ hello.go
@@ -1,10 +1,14 @@
-package main
+ package main
 
 import (
 	"fmt"      //pacote para Println e scanf
 	"net/http" //pacote para acessar internet
 	"os"       //pacote para erros e sair do aplicativo
 	"reflect"
+	"io/ioutil"//abrir arquivo
+	"bufio" //abrir arquivo
+	"time"
+	"strconv" //converter diversos tipos para string
 )
 
 const monitoramentos = 3
@@ -28,6 +32,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs")
+			imprimiLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -83,7 +88,8 @@ func leComando() int { //o retorno da funcao fica depois
 
 func iniciarMonitoramento() {
 	fmt.Println("iniciando monitoramento")
-	sites := []string{"https://www.alura.com.br", "https://www.google.com", "https://www.gmail.com"}
+	//sites := []string{"https://www.alura.com.br", "https://www.google.com", "//https://www.gmail.com"}
+	sites:= leSitesDoArquivos ()
 
 	for i, site := range sites {
 		fmt.Println("Testando site", i, ":", site)
@@ -96,12 +102,16 @@ func iniciarMonitoramento() {
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site) //err caso aconteça algum erro
-
+	resp, err := http.Get(site) //err caso aconteça algum erro
+	if err !=nill{
+			fmt.Println("Ocorreu um erro:",err)
+	}
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site,true)
 	} else {
 		fmt.Println("Site: ", site, "está com erros. Status Code:", resp.StatusCode)
+		registraLog(site,false)
 	}
 }
 
@@ -115,3 +125,51 @@ func exibeNomes() {
 func pulaLinha() {
 	fmt.Println("")
 }
+
+func leSitesDoArquivos ()[] string{
+	var sites[]string
+	arquivo,err:= os.Open("sites.txt")
+	//arquivo, err :=ioutil.ReadFile("sites.txt")// bom para ler tudo de uma vez
+	if err !=nil{
+		fmt.Println("Ocorreu um erro:", err)
+		
+	}
+	leitor := bufio.NewReader(arquivo)//para ler linha por linha
+	for{
+		linha, err:=leitor.ReadString('\n') //\n para ler ate o final
+		linha = strings.TrimSpace(linha) //para tirar os espacos do final do arquivo e tirar o /n
+		
+		sites = append(sites, linha)
+		
+		if err ==io.EOF{ //io.EOF erro de quando chega no final do arquivo/ ultima linha
+			break //se encontrar o final do arquivo ele sai do for
+			
+		}
+		
+	}
+	fmt.Println(sites)	
+	
+	arquivo.Close()
+	return sites
+	
+	}
+	
+	func registraLog(site string, status bool){
+		arquivo, err:= os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE|os.O_APPEND, 0666) //OpenFile consegue abrir arquivo, caso nao exista, ele pode abrir o arquivo e consegue escrever tambem, O_RDWR para pode escrever O_CREATE para poder criar caso nao tenha
+		O_APPEND para ele escrever na ultima linha
+		
+		if err !=nil{
+		fmt.Println(err)
+		}
+		arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05")+"-" + site +  "-online: "+ strconv.FormatBool(status)+"\n")
+		arquivo.Close()
+		
+	}
+	
+	func imprimiLogs(){
+		arquivo, err := ioutil.ReadFile("log.txt") //ele ja le o arquivo e ja fecha, entao nao precisa fechar
+		if err !=nil{
+			fmt.Println(err)
+		}
+		fmt.Println(string(arquivo))
+	}
